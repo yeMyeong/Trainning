db32420ccf5248d14d42bc0b3e3d53b662efe7c9
yeMyeong <88498506+yeMyeong@users.noreply.github.com>
2023-03-10 23:00:14 +0900

    Update CircleRotation.cs

--- CircleRotation.cs
+++ CircleRotation.cs
@@ -4,24 +4,17 @@ using UnityEngine;
 
 public class CircleRotation : MonoBehaviour
 {
-    float rotSpeed = 0.0f;
+    float rotSpeed = 0.0f; // 회전 속도
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        if(Input.GetMouseButton(0))
+        if(Input.GetMouseButton(0)) // 마우스를 좌클릭하면
         {
-            rotSpeed = 10;
+            rotSpeed = 10; // 회전 속도가 올라감
         }
 
-        transform.Rotate(0, 0, rotSpeed);
+        transform.Rotate(0, 0, rotSpeed); // Rotate(x, y, z) 를 사용하여 돌아감
 
-        rotSpeed *= 0.99f;
+        rotSpeed *= 0.99f; // 점점 속도가 느려짐
     }
 }
