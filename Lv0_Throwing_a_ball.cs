47443e0cd153185961b8bea8771243b24a73d832
yeMyeong <88498506+yeMyeong@users.noreply.github.com>
2023-03-16 23:31:28 +0900

    Create Lv0_Throwing_a_ball.cs

---
+++ Lv0_Throwing_a_ball.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lv0_Throwing_a_ball
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
