304d209d5d0515c13f808ea581f045f537733aed
yeMyeong <88498506+yeMyeong@users.noreply.github.com>
2023-03-17 23:52:36 +0900

    Update Test.py

--- Test.py
+++ Test.py
@@ -1,7 +1,15 @@
+# 프로그래머스 Lv0_짝수 홀수 개수
+
+# 정수가 담긴 리스트 num_list가 주어질 때,
+# num_list의 원소 중 짝수와 홀수의 개수를 담은 배열을 return 하도록 solution 함수를 완성해보세요.
+
 num_list = [1, 2, 3, 4, 5]
+answer = [0, 0]
 
-num_list.append(5)
+for n in num_list:
+    if n % 2 == 0:
+        answer[0] += 1
+    else:
+        answer[1] += 1
 
-del num_list[0]
-
-print(num_list)
+print(answer)
