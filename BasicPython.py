7f50e1d11538fd052df1e4dc075c28010bacf355
yeMyeong <88498506+yeMyeong@users.noreply.github.com>
2023-03-18 23:54:34 +0900

    Update BasicPython.py

--- BasicPython.py
+++ BasicPython.py
@@ -1,8 +1,10 @@
-# 
-
 # input, 형변환
-num = int(input("아무 숫자나 입력하세요"))
+num = int(input("아무 자연수나 입력하세요"))
 
 # if 문
-if num > 7:
-    print("n은 7보다 넘었습니다")
+if num > 1000:
+    print("n은 1000보다 큰 수 입니다")
+elif num > 100:
+    print("n은 1000 미만 100 초과입니다.")
+else:
+    print("n은 100 미만 0 이상입니다.")
