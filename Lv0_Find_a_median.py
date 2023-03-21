600709d3b4bc92604b51f90db62169818b460760
yeMyeong <88498506+yeMyeong@users.noreply.github.com>
2023-03-22 02:49:43 +0900

    1

---
+++ Lv0_Find_a_median.py
@@ -0,0 +1,4 @@
+array = [9, -1, 0]
+
+array.sort()
+print(array[len(array) // 2])
---
+++ Lv0_Share_a_pizza.py
@@ -0,0 +1,9 @@
+n = 7
+answer = 0
+
+if n % 7 == 0:
+    answer = n / 7
+else:
+    answer = n // 7 + 1
+
+print(answer)
