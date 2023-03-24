bb27f5cb79aa81ea926474bb5b6dbac53c70d3a5
yeMyeong <88498506+yeMyeong@users.noreply.github.com>
2023-03-25 02:33:24 +0900

    Create Lv0_Make_B_out_of_A.py

---
+++ Lv0_Make_B_out_of_A.py
@@ -0,0 +1,23 @@
+"""문자열 before와 after가 매개변수로 주어질 때,
+before의 순서를 바꾸어 after를 만들 수 있으면 1을,
+만들 수 없으면 0을 return 하도록 solution 함수를 완성해보세요."""
+
+before = "olleh"
+after = "hello"
+
+answer = 0
+count = 0
+
+for i in before:
+    #for j in after:
+        #if i == j:
+    after = after.replace(i, "", 1)            
+    #print(after)
+if after == "":
+    answer = 1
+else:
+    answer = -1
+
+print(answer)
+#after = after.replace("o", "")
+#print(after)
